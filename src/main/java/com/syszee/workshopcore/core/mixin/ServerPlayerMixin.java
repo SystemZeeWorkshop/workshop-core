649134f663ea6a0e40cfdbae7e0b11bc7d2b551a
SmellyModder <41268323+SmellyModder@users.noreply.github.com>
2023-01-03 19:47:45 -0500

    Fixed death confetti particles

--- src/main/java/com/syszee/workshopcore/core/mixin/ServerPlayerMixin.java
+++ src/main/java/com/syszee/workshopcore/core/mixin/ServerPlayerMixin.java
@@ -1,17 +1,31 @@
 package com.syszee.workshopcore.core.mixin;
 
+import com.mojang.authlib.GameProfile;
 import com.syszee.workshopcore.core.WCPlayer;
 import com.syszee.workshopcore.core.WCServerPlayer;
+import com.syszee.workshopcore.core.registry.WCGameRules;
+import com.syszee.workshopcore.core.registry.WCParticleTypes;
+import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.player.ProfilePublicKey;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ServerPlayer.class)
-public final class ServerPlayerMixin implements WCServerPlayer {
+public abstract class ServerPlayerMixin extends Player implements WCServerPlayer {
 	private boolean explosivePunchEnabled;
+
+	private ServerPlayerMixin(Level level, BlockPos blockPos, float f, GameProfile gameProfile, @Nullable ProfilePublicKey profilePublicKey) {
+		super(level, blockPos, f, gameProfile, profilePublicKey);
+	}
 
 	@Inject(method = "restoreFrom", at = @At("RETURN"))
 	private void restoreWCDataFrom(ServerPlayer oldPlayer, boolean bl, CallbackInfo info) {
@@ -29,6 +43,13 @@ public final class ServerPlayerMixin implements WCServerPlayer {
 	private void readWCSaveData(CompoundTag compoundTag, CallbackInfo info) {
 		this.explosivePunchEnabled = compoundTag.getBoolean("ExplosivePunchEnabled");
 		((WCPlayer) (Object) this).setFrozen(compoundTag.getBoolean("IsFrozen"));
+	}
+
+	@Inject(method = "die", at = @At("TAIL"))
+	private void die(DamageSource damageSource, CallbackInfo info) {
+		if (this.level instanceof ServerLevel serverLevel && serverLevel.getGameRules().getBoolean(WCGameRules.RULE_DO_DEATH_CONFETTI)) {
+			serverLevel.sendParticles(WCParticleTypes.CONFETTI, this.getX(), this.getY(), this.getZ(), this.random.nextInt(90, 120), 0.0D, 0.0D, 0.0D, 1.0D);
+		}
 	}
 
 	@Override
