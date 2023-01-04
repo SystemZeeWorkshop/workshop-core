edc1ee9a0ab13b538e6c8270c625a378ff1f9e9c
SmellyModder <41268323+SmellyModder@users.noreply.github.com>
2023-01-03 20:04:47 -0500

    Hide GUI effects on bodies

--- src/main/java/com/syszee/workshopcore/client/render/BodyRenderer.java
+++ src/main/java/com/syszee/workshopcore/client/render/BodyRenderer.java
@@ -3,6 +3,7 @@ package com.syszee.workshopcore.client.render;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.syszee.workshopcore.common.entity.Body;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.Options;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
@@ -19,7 +20,11 @@ public class BodyRenderer extends EntityRenderer<Body> {
 	public void render(Body body, float f, float g, PoseStack poseStack, MultiBufferSource multiBufferSource, int i) {
 		var entity = body.getEntity();
 		if (entity == null) return;
+		Options options = Minecraft.getInstance().options;
+		boolean oldHideGui = options.hideGui;
+		options.hideGui = true;
 		Minecraft.getInstance().getEntityRenderDispatcher().getRenderer(entity).render(entity, f, 0.0F, poseStack, multiBufferSource, i);
+		options.hideGui = oldHideGui;
 	}
 
 	@Override
